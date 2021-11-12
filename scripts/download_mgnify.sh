16f46b7663932c0189dc33156caacd91f1a4c276
harindaoci <80308072+harindaoci@users.noreply.github.com>
2021-11-12 13:32:26 +1100

    Update download_mgnify.sh

--- scripts/download_mgnify.sh
+++ scripts/download_mgnify.sh
@@ -33,7 +33,7 @@ DOWNLOAD_DIR="$1"
 ROOT_DIR="${DOWNLOAD_DIR}/mgnify"
 # Mirror of:
 # ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/peptide_database/2018_12/mgy_clusters.fa.gz
-SOURCE_URL="https://storage.googleapis.com/alphafold-databases/casp14_versions/mgy_clusters_2018_12.fa.gz"
+SOURCE_URL="https://objectstorage.us-ashburn-1.oraclecloud.com/n/idcxvbiyd8fn/b/mgnify/o/2018_12/mgy_clusters.fa.gz"
 BASENAME=$(basename "${SOURCE_URL}")
 
 mkdir --parents "${ROOT_DIR}"
